2fa89e75e3766d76f18fb7684ba59f5bc7b7fd29
Christian Zimmermann <christian.zimmermann@ur.de>
2017-03-01 18:25:26 +0100

    some standard index / range definitions

--- src/multi_array_header.h
+++ src/multi_array_header.h
@@ -1,3 +1,4 @@
+// -*- C++ -*-
 
 #ifndef __multi_array_header_h__
 #define __multi_array_header_h__
@@ -11,5 +12,34 @@
 #include "multi_array_operation.h"
 #include "multi_array.h"
 #include "slice.h"
+
+namespace MultiArrayTools
+{
+    
+    /*********************************
+     *   Some standard definitions   *
+     *********************************/
+
+    // ===== Index Types =====
+
+    typedef SingleIndex<size_t,RangeType::ANY> GenericNI;
+    typedef SingleIndex<double,RangeType::ANY> GenericFI;
+    typedef SingleIndex<size_t,RangeType::LORENTZ> LorentzI;
+    typedef SingleIndex<int,RangeType::SPACE> Space1dNI;
+    typedef SingleIndex<int,RangeType::MOMENTUM> Mom1dNI;
+    // ...
+
+    // ===== Range Types =====
+
+    typedef SingleRange<size_t,RangeType::ANY> GenericNR;
+    typedef SingleRange<double,RangeType::ANY> GenericFR;
+    typedef SingleRange<size_t,RangeType::LORENTZ> LorentzR;
+    typedef SingleRange<int,RangeType::SPACE> Space1dNR;
+    typedef SingleRange<int,RangeType::MOMENTUM> Mom1dNR;
+    typedef MultiRange<Space1dNR,Space1dNR,Space1dNR> Space3dNR;
+    typedef MultiRange<Mom1dNR,Mom1dNR,Mom1dNR> Mom3dNR;
+    // ...
+    
+}
 
 #endif
