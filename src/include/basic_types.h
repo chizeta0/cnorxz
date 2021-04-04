70c850be2f271fdf7357e8cea53c14f8b7d0cc85
Christian Zimmermann <christian.zimmermann@ur.de>
2021-04-04 14:21:09 +0200

    basic_types

---
+++ src/include/basic_types.h
@@ -0,0 +1,18 @@
+#ifndef __basic_types_h__
+#define __basic_types_h__
+
+#include <string>
+
+namespace MultiArrayTools
+{
+
+    // just to fix this forever:
+    typedef int32_t Int;
+    typedef uint64_t SizeT;
+    typedef double Double;
+    typedef Double Real;
+    typedef std::string String;
+    
+}
+
+#endif
