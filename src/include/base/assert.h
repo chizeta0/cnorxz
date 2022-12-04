13467b800a867c34a361576588c7be57393c4492
Christian Zimmermann <christian.zimmermann@ur.de>
2022-12-04 21:57:18 +0100

    fix cxz error macro (clang)

--- src/include/base/assert.h
+++ src/include/base/assert.h
@@ -5,9 +5,8 @@
 #define CXZ_ERRTAG __FILE__ << '@' << __LINE__ << '(' << __func__ << "): error"
 #define CXZ_WARNTAG __FILE__ << '@' << __LINE__ << ": warning"
 #define CXZ_ERROR(errmsg) {\
-    std::stringstream ss;\
-    ss << CXZ_ERRTAG << ": " << errmsg << std::flush;\
-    throw std::runtime_error(ss.str()); }
+    auto mkerr = [&](){ std::stringstream ss; ss << CXZ_ERRTAG << ": " << errmsg << std::flush; return ss.str(); }; \
+    throw std::runtime_error(mkerr()); }
 
 #define CXZ_WARNING(errmsg) {\
     std::cerr << CXZ_WARNTAG << ": " << errmsg << std::endl; }
